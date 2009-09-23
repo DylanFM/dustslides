ac276b2ae6a031ada92a56e19edfce017eec6e12
DylanFM <dylan.fm@gmail.com>
2009-09-23 23:08:46 +1000

    Changed time. New file with results.

--- dustslides.rb
+++ dustslides.rb
@@ -2,7 +2,7 @@ require 'net/http'
 require 'uri'
 
 def existing_frames
-  (600..1800).map do |time|
+  (500..1800).map do |time|
     response = nil
     Net::HTTP.start("strm-gallery.coastalwatch.com", 80) do |http|
       response = http.head("/camera/livestill/227/LiveStill_200909230#{time}.gif")
